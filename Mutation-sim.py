ac7205a6908e950c3a3594051f7586c0f026b08c
kursatozer <f.kursatozer@gmail.com>
2023-05-19 01:30:46 +0300

    Creating the output directory

--- Mutation-sim.py
+++ Mutation-sim.py
@@ -1,3 +1,6 @@
 import random
 import os
 import shutil
+
+def mutate_genomes(input_directory, output_directory, mutation_rate):
+    os.makedirs(output_directory, exist_ok=True)  # Creating the output directory

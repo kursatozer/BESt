f627d0858aaccea76edd7c246e2b9ea7b9a33123
kursatozer <f.kursatozer@gmail.com>
2023-05-19 01:46:45 +0300

    saved massage added

--- Mutation-sim.py
+++ Mutation-sim.py
@@ -34,3 +34,5 @@ def mutate_genomes(input_directory, output_directory, mutation_rate):
             output_file = os.path.join(output_directory, file_name)
             with open(output_file, 'w') as file:
                 file.write('\n'.join(original_genome_lines + mutated_genome_lines))  # Write mutated genome lines to new location
+
+            print(f"The mutated genome has been successfully saved to '{output_file}'.")

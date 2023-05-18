c92c0f5047969e1ae9c6b944aa62e3d85e8dc408
kursatozer <f.kursatozer@gmail.com>
2023-05-19 01:45:15 +0300

    location folder create command added

--- Mutation-sim.py
+++ Mutation-sim.py
@@ -28,3 +28,9 @@ def mutate_genomes(input_directory, output_directory, mutation_rate):
                             mutated_line += base
                     else:
                         mutated_line += base
+
+                mutated_genome_lines.append(mutated_line.strip())
+
+            output_file = os.path.join(output_directory, file_name)
+            with open(output_file, 'w') as file:
+                file.write('\n'.join(original_genome_lines + mutated_genome_lines))  # Write mutated genome lines to new location

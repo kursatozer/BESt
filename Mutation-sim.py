d831ad9ad2d0538c943c6813812c9c72b6abe393
kursatozer <f.kursatozer@gmail.com>
2023-05-19 01:32:09 +0300

    List the genome files in the home directory

--- Mutation-sim.py
+++ Mutation-sim.py
@@ -4,3 +4,5 @@ import shutil
 
 def mutate_genomes(input_directory, output_directory, mutation_rate):
     os.makedirs(output_directory, exist_ok=True)  # Creating the output directory
+
+    genome_files = os.listdir(input_directory)  # List the genome files in the home directory

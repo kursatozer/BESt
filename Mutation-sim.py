7428d34039a8e4d0bf5e505adc1b18aae7ccdaef
kursatozer <f.kursatozer@gmail.com>
2023-05-19 01:34:11 +0300

    Processing .fna files only

--- Mutation-sim.py
+++ Mutation-sim.py
@@ -6,3 +6,7 @@ def mutate_genomes(input_directory, output_directory, mutation_rate):
     os.makedirs(output_directory, exist_ok=True)  # Creating the output directory
 
     genome_files = os.listdir(input_directory)  # List the genome files in the home directory
+
+    for file_name in genome_files:
+        if file_name.endswith('.fna'):  # Processing .fna files only
+            input_file = os.path.join(input_directory, file_name)

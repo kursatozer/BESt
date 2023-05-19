496ab96ca0264de858c90cbd82ff9c232d1fbf49
kursatozer <f.kursatozer@gmail.com>
2023-05-19 03:05:58 +0300

    def

--- Mutation-sim.py
+++ Mutation-sim.py
@@ -2,7 +2,7 @@ import random
 import os
 import shutil
 
-def mutate_genomes(input_directory, output_directory, mutation_rate):
+def mutate_genomes(input_directory, output_directory, mutation_rate, frame_shift_rate):
     os.makedirs(output_directory, exist_ok=True)  # Creating the output directory
 
     genome_files = os.listdir(input_directory)  # List the genome files in the home directory

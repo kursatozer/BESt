f1f52b583159e893f14ad7737c05b4964ba043ec
kursatozer <f.kursatozer@gmail.com>
2023-05-19 03:27:55 +0300

    Apply frameshift mutation added

--- Mutation-sim.py
+++ Mutation-sim.py
@@ -3,18 +3,18 @@ import os
 import shutil
 
 def mutate_genomes(input_directory, output_directory, mutation_rate, frame_shift_rate):
-    os.makedirs(output_directory, exist_ok=True)  # Creating the output directory
+    os.makedirs(output_directory, exist_ok=True) # Creating the output directory
 
-    genome_files = os.listdir(input_directory)  # List the genome files in the home directory
+    genome_files = os.listdir(input_directory) # List the genome files in the home director
 
     for file_name in genome_files:
-        if file_name.endswith('.fna'):  # Processing .fna files only
+        if file_name.endswith('.fna'): # Processing .fna files only
             input_file = os.path.join(input_directory, file_name)
 
             with open(input_file, 'r') as file:
-                lines = file.readlines()  # Read file line by line
+                lines = file.readlines() # Read file line by line
 
-            original_genome_lines = [line.strip() for line in lines]  # Get first line and skip other lines
+            original_genome_lines = [line.strip() for line in lines] # Get first line and skip other lines
 
             mutated_genome_lines = []
             for line in original_genome_lines:
@@ -22,8 +22,9 @@ def mutate_genomes(input_directory, output_directory, mutation_rate, frame_shift
                 for base in line:
                     if base in ['A', 'T', 'G', 'C']:
                         if random.random() < mutation_rate:
-                            base = random.choice('ATGC')  # We randomly select a new nucleotide out of the current nucleotide
+                            base = random.choice('ATGC') # We randomly select a new nucleotide out of the current nucleotide
                     mutated_line.append(base)
+
                 # Apply frameshift mutation
                 if random.random() < frame_shift_rate and len(mutated_line) > 0:
                     last_base = mutated_line[-1]
@@ -33,19 +34,15 @@ def mutate_genomes(input_directory, output_directory, mutation_rate, frame_shift
 
             output_file = os.path.join(output_directory, file_name)
             with open(output_file, 'w') as file:
-                file.write('\n'.join(mutated_genome_lines))
-            with open(output_file, 'w') as file:
-                file.write('\n'.join(original_genome_lines + mutated_genome_lines))  # Write mutated genome lines to new location
+                file.write('\n'.join(mutated_genome_lines)) # Write mutated genome lines to new location
 
-            print(f"The mutated genome has been successfully saved to '{output_file}'.")
+            print(f"Mutasyona uğramış genom başarıyla '{output_file}' konumuna kaydedildi.")
 
-    print("Whole genomes have been mutated to new locations")
+    print("Tüm bakteri genomları mutasyona uğratılarak yeni konumlara kaydedildi.")
 
-input_directory = './original_genomes'
+input_directory = '/home/kratzer/Desktop/aMeta-sim/cont'
+output_directory = 'mutated_genomes'
 mutation_rate = 0.03
-frame_shift_rate = 0.01
+frame_shift_rate = 0.05
 
 mutate_genomes(input_directory, output_directory, mutation_rate, frame_shift_rate)
-
-
-mutate_genomes(input_directory, output_directory, mutation_rate)
